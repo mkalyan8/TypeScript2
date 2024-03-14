e3b3d486957356240329a30a8546ad5790f58180
Kalyan <mkalyan8@gmail.com>
2024-03-14 11:29:30 +0530

    First commit

---
+++ ArrowFunctions.js
@@ -0,0 +1,38 @@
+// Arrow Function
+var sum = function (x, y) {
+    return x + y;
+};
+console.log(sum(83, 78));
+// Parameterless Arroe Functin
+var p = function () {
+    console.log("Welcome : ");
+};
+p();
+yjukjl
+/*
+    If the function body consists of only statement
+    then no need for the curly brackets and the return keyword
+*/
+var Add = function (i, j) {
+    return i + j;
+};
+console.log(Add(32, 97));
+var Addition = function (k, l) { return k + l; };
+console.log(Addition(34, 677));
+function A(m, n) {
+    return m + n;
+}
+console.log(A(34, 76));
+console.log(A("Kalyan ", "Mysa"));
+// Function overloading with different number of parameters and
+// types with same naem is not supported
+/*
+Rest Parameters:
+---------------
+If number of parameters that a function will receive is not known or acn vary, we can use rest parameters
+We can use the rest parametrs denoted by ellipsis, denoted by ... (3 dots)
+
+
+
+
+*/
